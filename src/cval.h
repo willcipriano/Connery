0d37b4ca1377ed518e573ed18bdf56138487e0ab
Will Cipriano <will@willcipriano.com>
2020-08-19 21:27:16 -0400

    Organize cval.h

--- src/cval.h
+++ src/cval.h
@@ -34,6 +34,7 @@ struct cenv {
 };
 
 char* ctype_name(int t);
+
 cval* cval_function(cbuiltin func);
 cval* cval_number(long x);
 cval* cval_float(long double x);
@@ -42,26 +43,15 @@ cval* cval_error(char* fmt, ...);
 cval* cval_symbol(char* s);
 cval* cval_s_expression(void);
 cval* cval_q_expression(void);
-cenv* cenv_new(void);
-void cenv_delete(cenv* e);
 void cval_delete(cval* value);
 cval* cval_take(cval* value, int i);
 cval* cval_pop(cval* value, int i);
-cval* builtin_eval(cenv* e, cval* a);
-cenv* cenv_copy(cenv* e);
 cval* cval_copy(cval* v);
-void cenv_put(cenv* e, cval* k, cval* v);
-cval* builtin_list(cenv* e, cval* a);
 cval* cval_add(cval* v, cval* x);
 cval* cval_call(cenv* e, cval* f, cval* a);
 cval* cval_evaluate_s_expression(cenv* env, cval* value);
 cval* cenv_get(cenv* e, cval* k);
 cval* cval_evaluate(cenv* env, cval* value);
-void cenv_add_builtin(cenv* e, char* name, cbuiltin func);
-void cenv_def(cenv* e, cval* k, cval* v);
-cval* builtin_var(cenv* e, cval* a, char* func);
-cval* builtin_put(cenv* e, cval* a);
-cval* builtin_def(cenv* e, cval* a);
 cval* cval_join(cval* x, cval* y);
 cval* cval_read_num(mpc_ast_t* t);
 cval* cval_read_float(mpc_ast_t* t);
@@ -69,4 +59,17 @@ cval* cval_read(mpc_ast_t* t);
 void cval_print_line(cval* value);
 void cval_expr_print(cval* value, char open, char close);
 void cval_print(cval* value);
+
+cenv* cenv_new(void);
+void cenv_add_builtin(cenv* e, char* name, cbuiltin func);
+void cenv_def(cenv* e, cval* k, cval* v);
+void cenv_delete(cenv* e);
+void cenv_put(cenv* e, cval* k, cval* v);
+cenv* cenv_copy(cenv* e);
+
+cval* builtin_var(cenv* e, cval* a, char* func);
+cval* builtin_put(cenv* e, cval* a);
+cval* builtin_def(cenv* e, cval* a);
+cval* builtin_eval(cenv* e, cval* a);
+cval* builtin_list(cenv* e, cval* a);
 #endif //CONNERY_CVAL_H
