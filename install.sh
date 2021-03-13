ed3539c92d0eb639b43b2df3c0382d2d7e09b004
Will Cipriano <will@willcipriano.com>
2021-03-13 01:56:03 -0500

    added install script

--- install.sh
+++ install.sh
@@ -2,8 +2,7 @@
 
 export CONNERY_PATH=$(eval echo ~$user)"/Connery";
 
-
-(cd ../"$CONNERY_PATH" || exit ; git clone --single-branch --branch release https://github.com/willcipriano/Connery.git) || true ;
+(cd "$CONNERY_PATH" || exit ; cd ../ && git clone --single-branch --branch release https://github.com/willcipriano/Connery.git) || true ;
 (cd "$CONNERY_PATH" || exit ; make -f Buildfile local.install ; make -f Buildfile local.build && make -f Buildfile local.run)
 
 
